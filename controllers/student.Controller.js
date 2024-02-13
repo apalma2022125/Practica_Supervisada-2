8e6c51a9b19fcb52f69cc80ea359e50ba2ee8788
Angelo_Palma <apalma-2022125@kinal.edu.gt>
2024-02-13 15:26:20 -0600

    Se finalizo el controller de Student

---
+++ controllers/student.Controller.js
@@ -0,0 +1,79 @@
+const bcrypt = require('bcrypt');
+const Student = require('../models/student');
+const {response, request} = require('express');
+
+const studentGet = async (req, res = response) =>{
+    const {limite, desde} = req.query;
+    const query = {estado: true};
+
+    const [total, students] = await Promise.all([
+        Student.countDocuments (query),
+        Student.find(query)
+        .skip(Number(limite))
+    ]);
+
+    res.status(200).json({
+        toral,
+        students
+    });
+}
+
+    const getStudentById = async (req, res) =>{
+        const {id} = req.params;
+        const student= await Student.findOne({_id: id});
+
+        res.status(200).json({
+            student
+        });
+    }
+
+    const putStudents = async (req, res = response) =>{
+    const {id} = req.params;
+    const {_id, password,email, ...resto} = req.body;
+
+    if(password){
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const student = await Student.findByIdAndUpdate(id, {estado: false});
+
+    res.status(200).json({
+        msg: 'The student has been successfully upgraded',
+        student
+    });
+}
+
+const studentDelete = async (req, res) =>{
+    const {id} = req.params;
+    const student = await Student.findByIdAndUpdate(id, {estado:false});
+
+    res.status(200).json({
+        msg: 'The student has been successfully removed',
+        student
+    });
+}
+
+const studentPost = async (req, res) => {
+    const {name, email, password, role} = req.body;
+    const student = new Student({name, email, password, role});
+
+    const salt = bcryptjs.genSaltSync();
+    student.password = bcryptjs.hashSync(password,salt);
+
+    await student.save();
+    res.status(202).json({
+        student
+    });
+}
+
+module.export = {
+
+    studentPost,
+    studentGet,
+    getStudentById,
+    putStudents,
+    studentDelete    
+}
+
+
