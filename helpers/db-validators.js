6cc4252f4025a0f09ad11c850358948e0cffd718
apalma2022125 <apalma-2022125@kinal.edu.gt>
2024-02-18 18:56:16 -0600

    Se terminaron las validaciones

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,5 +1,7 @@
-const Student = require('../models/student')
+const Student = require('../models/student');
 const Teacher = require('../models/teacher');
+const Courses = require('../models/courses');
+const mongoose = require('mongoose');
 
 const existingEmail = async (email = '') =>{
     const existEmail = await Student.findOne({email});
@@ -30,9 +32,56 @@ const existingTeacherById = async (id = '') =>{
     }
 }
 
+
+const isCourseValid = async (courses = []) => {
+    for (const id of courses){
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            throw new Error(`The id '${id}' is not valid`);
+        }
+
+        const courseExisting = await Courses.findOne({ _id: id });
+
+        if (!courseExisting) {
+            throw new Error(`The course with id:${id} does not exist in the database`);
+        }
+    }
+};
+
+const DuplicateCourses = async (courses = []) => {
+    const uniqueCourseSet = new Set();
+
+    for (const course of courses) {
+        if (uniqueCourseSet.has(course)) {
+            throw new Error(`Duplicate course detected: ${course}`);
+        }
+        uniqueCourseSet.add(course);
+    }
+};
+
+const MaxCourses = async (courses = []) => {
+    if (courses.length > 3) {
+        throw new Error(`You can select up to 3 courses. Please remove any extra courses.`);
+    }
+};
+
+const maxCuorses = async (Courses = []) => {
+    const maxCourses = 3;
+
+    if (Courses.length > maxCourses) {
+        throw new Error(`You can only select up to ${maxCourses} courses.`);
+    }
+};
+
+
+
+
 module.exports = {
     existingStudentById,
     existingEmail,
     existingEmailTeacher,
-    existingTeacherById
+    existingTeacherById,
+    isCourseValid,
+    DuplicateCourses,
+    MaxCourses,
+    maxCuorses
 }
